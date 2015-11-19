2042df710a20087f772d8ebf62cef285832545f1
Noah Lordi <n.p.lordi@gmail.com>
2015-11-19 16:45:32 -0700

    version 1.1.7

--- Dean's  office get rekt/src/org/usfirst/frc/team3729/robot/ultrasonicsensor.java
+++ Dean's  office get rekt/src/org/usfirst/frc/team3729/robot/ultrasonicsensor.java
@@ -1,5 +1,29 @@
 package org.usfirst.frc.team3729.robot;
 
-public class ultrasonicsensor {
+import edu.wpi.first.wpilibj.Ultrasonic;
+import edu.wpi.first.wpilibj.Ultrasonic.Unit;
 
+public class ultrasonicsensor {
+	Ultrasonic sonicfront;
+	double range;
+	robotDrive drive;
+	public ultrasonicsensor()
+	{
+		drive = new robotDrive();
+		sonicfront = new Ultrasonic(9,8,Unit.kMillimeter);
+		sonicfront.setEnabled(true);
+	}
+	public void wallHacks()
+	{
+		range = sonicfront.getRangeMM();
+		if (sonicfront.isRangeValid()==true)
+		{
+			if (range<1000)
+			{
+				drive.mcLeft.set(0);
+				drive.mcRight.set(0);
+				
+			}
+		}
+	}
 }
